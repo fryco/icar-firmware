5a64ad45afdbe83ebf50b39065c1aa47bdfbe579
cn0086.info <cn0086.info@gmail.com>
2012-04-16 07:21:37 +0000

    Start 0x8001000

---
+++ STM32/fw_v06/source/App/drv_mg323.h
@@ -0,0 +1,34 @@
+/**
+  ******************************************************************************
+  * SVN revision information:
+  * @file    $URL: https://icar-firmware.googlecode.com/svn/STM32/v040/source/App/drv_mg323.h $ 
+  * @version $Rev: 65 $
+  * @author  $Author: cn0086.info@gmail.com $
+  * @date    $Date: 2012-02-08 15:08:27 +0800 (周三, 2012-02-08) $
+  * @brief   This is GSM module: MG323 driver
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __DRV_MG323_H
+#define __DRV_MG323_H
+
+/* Exported macro ------------------------------------------------------------*/
+
+/* Includes ------------------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+
+/* Exported constants --------------------------------------------------------*/
+
+/* Exported functions ------------------------------------------------------- */
+unsigned char cmpmem(unsigned char *, unsigned char *,unsigned char );
+unsigned char gsm_power_on( void ) ;
+unsigned char gsm_check_gprs( void );
+bool gprs_disconnect( DISCONNECT_REASON );
+bool gsm_pwr_off( POWEROFF_REASON );
+unsigned char check_gsm_CSQ( void );
+unsigned char check_tcp_status( void );
+bool get_respond( unsigned char* rec_str);
+bool gsm_send_tcp( unsigned char *send_buf, unsigned int dat_len );
+bool gsm_ask_tcp( unsigned int dat_len ) ;
+#endif /* __DRV_MG323_H */
