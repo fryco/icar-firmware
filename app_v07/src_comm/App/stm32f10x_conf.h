81b82a86a6c15c5358aea7244d10a5f21b0c3295
cn0086.info@gmail.com <cn0086.info@gmail.com@946678be-dd1f-1524-4c66-cc28db898971>
2012-05-08 04:34:32 +0000

    Compiler setting

--- app_v07/src_comm/App/stm32f10x_conf.h
+++ app_v07/src_comm/App/stm32f10x_conf.h
@@ -28,7 +28,7 @@
 /* #include "stm32f10x_dac.h" */
 /* #include "stm32f10x_dbgmcu.h" */
 #include "stm32f10x_dma.h"
-#include "stm32f10x_exti.h"
+//#include "stm32f10x_exti.h"
 #include "stm32f10x_flash.h"
 /*#include "stm32f10x_fsmc.h"*/
 #include "stm32f10x_gpio.h"
@@ -38,8 +38,8 @@
 #include "stm32f10x_rcc.h"
 #include "stm32f10x_rtc.h"
 /* #include "stm32f10x_sdio.h" */
-#include "stm32f10x_spi.h"
-#include "stm32f10x_tim.h"
+//#include "stm32f10x_spi.h"
+//#include "stm32f10x_tim.h"
 #include "stm32f10x_usart.h"
 /*#include "stm32f10x_wwdg.h"*/
 #include "misc.h" /* High level functions for NVIC and SysTick (add-on to CMSIS functions) */
