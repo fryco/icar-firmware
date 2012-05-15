0c8609d52915200d4755acaa231cf5e86caeca18
cn0086.info <cn0086.info@gmail.com>
2012-05-15 01:36:43 +0000

    Stable revision, support OTA, for Dual LED

--- app_v07/src_comm/App/drv_mg323.c
+++ app_v07/src_comm/App/drv_mg323.c
@@ -8,7 +8,7 @@
 
 extern struct ICAR_DEVICE my_icar;
 
-const unsigned char dest_server[] = "cqt.8866.org:25";
+const unsigned char dest_server[] = "cqt.8866.org:23";
 const unsigned char at_set_channel0_para[] = "AT^SICS=0,";
 //  Init sequence:
 //  AT
