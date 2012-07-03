6e499eff96a50c790e2003e92a918c2a44eeb476
cn0086.info <cn0086.info@gmail.com>
2012-07-03 05:20:44 +0000

    Add property

--- ubuntu/iCar_v03/cloud_post.h
+++ ubuntu/iCar_v03/cloud_post.h
@@ -1,10 +1,10 @@
 /**
   ******************************************************************************
   * SVN revision information:
-  * @file    $URL: https://icar-firmware.googlecode.com/svn/ubuntu/iCar_v03/commands.h $ 
-  * @version $Rev: 99 $
+  * @file    $URL$ 
+  * @version $Rev$
   * @author  $Author$
-  * @date    $Date: 2012-02-29 09:32:56 +0800 (Wed, 29 Feb 2012) $
+  * @date    $Date$
   * @brief   This file is for cloud_post
   ******************************************************************************
   */ 
