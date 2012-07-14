188ae55e1855f4d2c7f32ab2f8dadc56a9b77a2d
cn0086.info <cn0086.info@gmail.com>
2012-07-14 10:05:01 +0000

    Modify cloud post information, support forum id

--- ubuntu/iCar_v03/database.c
+++ ubuntu/iCar_v03/database.c
@@ -920,9 +920,9 @@ int ask_instruction(struct icar_data *mycar, unsigned char *buf, unsigned char *
 	unsigned long sqlrow_cnt = 0 ;
 
 	// *ist = 0x55 ;//Upgrade firmware
-	 *ist = 0x75 ;//Update parameter
+	// *ist = 0x75 ;//Update parameter
 
-	//*ist = 0 ;//No new instruction
+	*ist = 0 ;//No new instruction
 
 	//input: HEAD+SEQ+PCB+LEN+CHK
 
