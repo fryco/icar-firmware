0495c3f3582ef739ac9fd8fc0cabe3f163263c14
cn0086.info@gmail.com <cn0086.info@gmail.com@946678be-dd1f-1524-4c66-cc28db898971>
2012-09-07 09:24:58 +0000

    Start auto_detect_obd

--- app_v07/src_comm/App/drv_can.h
+++ app_v07/src_comm/App/drv_can.h
@@ -22,7 +22,7 @@
 
 /* Exported functions ------------------------------------------------------- */
 //void can_init(void);
-
+void can_rec_all_id( bool );
 void can_init( can_speed_typedef ,  can_std_typedef  );
 bool can_send( can_std_typedef can_typ, frame_typedef frame_typ, u32 can_id, u8 dat_len, u8 * dat );
 #endif /* __DRV_CAN_H */
