c6f8c1ef1bd90ba335ce396da14868bd1150e314
cn0086.info@gmail.com <cn0086.info@gmail.com@946678be-dd1f-1524-4c66-cc28db898971>
2012-09-07 01:19:16 +0000

    Dev CAN, filer setting OK

--- app_v07/src_comm/App/app_obd.c
+++ app_v07/src_comm/App/app_obd.c
@@ -7,15 +7,24 @@ CanRxMsg RxMessage;
 
 extern OS_EVENT 	*sem_obd	;
 
+//warn code define, internal use only
+#define	UNK_CAN_STDID		10	//unknow CAN STD ID
+#define	UNK_CAN_EXTID_H		20	//Unknow ext-stand CAN ID High
+#define	UNK_CAN_EXTID_L		30	//Unknow ext-stand CAN ID low
+
 static void auto_detect_obd( void );
 
 //为了提高任务效率，改用事件驱动方式
 //
 void  app_task_obd (void *p_arg)
 {
+	u8 var_uchar;
+	
 	CPU_INT08U	os_err;
 
 	(void)p_arg;
+
+	OSSemPend(sem_obd, 0, &os_err);//Wait task manger ask to start
 
 	//BKP_DR2, OBD Flag:	15~12:KWP_TYPEDEF
 	//						11~8:CAN2_TYPEDEF
@@ -26,24 +35,24 @@ void  app_task_obd (void *p_arg)
 
 	switch ( obd_type ) {
 	case OBD_TYPE_CAN1 ://CAN1
-		debug_obd("OBD is CAN bus...\r\n");
+		debug_obd("OBD is CAN bus\r\n");
 		break;
 
 	case OBD_TYPE_NO_DFN ://no define
 	default://maybe error, treat as no define
-		debug_obd("No OBD type\t");
+		debug_obd("No OBD type\r\n");
 		BKP_WriteBackupRegister(BKP_DR2, 0);//clean all flag, prevent err
 		auto_detect_obd( );
 		
 		break;
 	}
 			
-	uart3_init( ); //to OBD, K-BUS
+	//uart3_init( ); //to OBD, K-BUS
 
 	//can_init( ); //to OBD, CAN BUS
 
 	while ( 1 ) {
-
+		debug_obd("OBD task Pend...\r\n");
 		OSSemPend(sem_obd, 0, &os_err);
 		if ( !os_err ) {
 
@@ -75,7 +84,57 @@ void  app_task_obd (void *p_arg)
 			}
 
 			//FIFO1 receive unknow CAN ID, then report to server
-			//check CAN1_RX1_IRQHandler(void) in drv_can.c
+			//FIFO0 has data, max.: 3 datas
+			if ( CAN_MessagePending(CAN1,CAN_FIFO1) ) {
+
+				CAN_Receive(CAN1,CAN_FIFO1, &RxMessage);
+				
+				if (RxMessage.IDE == CAN_ID_STD) { //unknow CAN STD ID, report to server
+					//prompt("CAN FIFO_1: %d ID: %X\r\n", CAN_MessagePending(CAN1,CAN_FIFO1),RxMessage.StdId);
+					debug_obd("Unknow CAN STD: %X FIFO:%d\r\n",RxMessage.StdId,CAN_MessagePending(CAN1,CAN_FIFO1));
+					
+					for ( var_uchar = 0 ; var_uchar < MAX_WARN_MSG ; var_uchar++) {
+						if ( !my_icar.warn[var_uchar].msg ) { //empty msg	
+							//unsigned int msg;//file name(1 Byte), msg(1 Byte), line(2 B)
+							my_icar.warn[var_uchar].msg = (F_DRV_can) << 24 ;
+							my_icar.warn[var_uchar].msg |= UNK_CAN_STDID << 16 ;//unknow CAN STD ID
+							my_icar.warn[var_uchar].msg |= RxMessage.StdId ;
+							var_uchar = MAX_WARN_MSG ;//end the loop
+						}
+					}// 验证成功
+				}
+				else { //unknow CAN EXT ID, report to server
+					//prompt("CAN FIFO_1: %d ID: %X\r\n", CAN_MessagePending(CAN1,CAN_FIFO1),RxMessage.ExtId);
+					debug_obd("Unknow CAN EXT: %X FIFO:%d\r\n",RxMessage.ExtId,CAN_MessagePending(CAN1,CAN_FIFO1));
+
+					for ( var_uchar = 0 ; var_uchar < MAX_WARN_MSG ; var_uchar++) {
+						if ( !my_icar.warn[var_uchar].msg ) { //empty msg	
+							//unsigned int msg;//file name(1 Byte), msg(1 Byte), line(2 B)
+							my_icar.warn[var_uchar].msg = (F_DRV_can) << 24 ;
+							my_icar.warn[var_uchar].msg |= UNK_CAN_EXTID_H << 16 ;//Unknow ext-stand CAN ID high
+							my_icar.warn[var_uchar].msg |= ( RxMessage.ExtId >> 16) &0xFFFF ;
+							var_uchar = MAX_WARN_MSG ;//end the loop
+						}
+					}// 验证成功 2012/8/28 10:58:10
+			
+					for ( var_uchar = 0 ; var_uchar < MAX_WARN_MSG ; var_uchar++) {
+						if ( !my_icar.warn[var_uchar].msg ) { //empty msg	
+							//unsigned int msg;//file name(1 Byte), msg(1 Byte), line(2 B)
+							my_icar.warn[var_uchar].msg = (F_DRV_can) << 24 ;
+							my_icar.warn[var_uchar].msg |= UNK_CAN_EXTID_L << 16 ;//Unknow ext-stand CAN ID low
+							my_icar.warn[var_uchar].msg |= ( RxMessage.ExtId ) &0xFFFF ;
+							var_uchar = MAX_WARN_MSG ;//end the loop
+						}
+					}// 验证成功 2012/8/28 10:58:08
+				}
+  				
+				if ( CAN_MessagePending(CAN1,CAN_FIFO1)==0 ) { //no data
+					CAN_ITConfig(CAN1,CAN_IT_FMP1, ENABLE);//enable FIFO1 FMP int
+				}
+				else { //still has data
+					OSSemPost( sem_obd );//no verify
+				}
+			}			
 		}
 	}
 }
@@ -83,7 +142,8 @@ void  app_task_obd (void *p_arg)
 static void auto_detect_obd( void )
 {
 	u8 stream_support[]="\x02\x01\x00\x00\x00\x00\x00\x00";
-	
+
+	//流程：开所有ID接收==>设波特率==>查看CAN_MessagePending有无数据
 	//1, Try CAN1_TYPE_STD_250
 	//debug_obd("Try CAN1,STD_250\r\n");
 	//can_init( CAN_250K, CAN_STD );
@@ -96,7 +156,7 @@ static void auto_detect_obd( void )
 	debug_obd("Try CAN1,STD_500\r\n");
 	can_init( CAN_500K, CAN_STD );
 	can_send( CAN_STD, DAT_FRAME, 0x07E0, 8, stream_support );
-	
+
 	//save to flag
 	return ;
 
--- app_v07/src_comm/App/drv_can.c
+++ app_v07/src_comm/App/drv_can.c
@@ -9,10 +9,8 @@ extern CanTxMsg TxMessage;
 //extern unsigned int rx_msg_cnt0, rx_msg_cnt1 ;
 
 //warn code define, internal use only
-#define	FIFO0_OF			01	//FIFO0 over flow
-#define	UNK_CAN_STDID		02	//unknow CAN STD ID
-#define	UNK_CAN_EXTID_H		03	//Unknow ext-stand CAN ID High
-#define	UNK_CAN_EXTID_L		04	//Unknow ext-stand CAN ID low
+#define	FIFO0_OF			10	//FIFO0 over flow
+#define	FIFO1_OF			20	//FIFO0 over flow
 
 void can_init( can_speed_typedef can_spd,  can_std_typedef can_typ )
 {
@@ -90,7 +88,13 @@ void can_init( can_speed_typedef can_spd,  can_std_typedef can_typ )
 	//CAN_FilterScale: CAN_FilterScale_16bit or CAN_FilterScale_32bit
 	CAN_FilterInitStructure.CAN_FilterScale = CAN_FilterScale_32bit;
 
-	//0x0000 ~ 0xFFFF
+	//如果Filter=0xF0， Mask=0x00，那么可以收到0xF0-0xFF共16个ID的信息。
+	//还是Filter=0xF0， Mask=0xFF，那么只能收到0xF0这一个ID信息。
+	//还是Filter=0xF0， Mask=0xF8，那么可以收到0xF8-0xFF共8个ID的信息。
+	//还是Filter=0xF0， Mask=0xFC，那么可以收到0xFC-0xFF共4个ID的信息。
+	//即 Mask对应位=1时，接收到的信息ID对应位必须=Filter的对应位才能确认处理
+    //   Mask对应位=0时，Filter对应位=1时，接收到的信息ID对应位必须=1才能确认处理
+    //   Mask对应位=0时，Filter对应位=0时，接收到的信息ID对应位=0，=1都能确认处理
 	if ( can_typ == CAN_EXT ) { //0x18DAF111
 		CAN_FilterInitStructure.CAN_FilterIdHigh = ((0x18DAF111)>>16)&0x1FFF;//29 bits,左对齐
 		CAN_FilterInitStructure.CAN_FilterIdLow = 0x18DAF111&0xFFFF;
@@ -98,9 +102,15 @@ void can_init( can_speed_typedef can_spd,  can_std_typedef can_typ )
 		CAN_FilterInitStructure.CAN_FilterMaskIdLow = 0xFFFF;
 	}
 	else {
-		CAN_FilterInitStructure.CAN_FilterIdHigh = (0x7FD)<<5;//11 bits,左对齐
+		//可通过CAN_ID:0x700~0x7FF
+		CAN_FilterInitStructure.CAN_FilterIdHigh = (0x700)<<5;//11 bits,左对齐
+		CAN_FilterInitStructure.CAN_FilterMaskIdHigh = (0x700)<<5;
+
+		//可通过CAN_ID:0x7F0~0x7FF
+		//CAN_FilterInitStructure.CAN_FilterIdHigh = (0x7F0)<<5;//11 bits,左对齐
+		//CAN_FilterInitStructure.CAN_FilterMaskIdHigh = (0x700)<<5;//可通过CAN_ID:0x7F0~0x7FF
+
 		CAN_FilterInitStructure.CAN_FilterIdLow = 0x0000;
-		CAN_FilterInitStructure.CAN_FilterMaskIdHigh = 0x7FF0;
 		CAN_FilterInitStructure.CAN_FilterMaskIdLow = 0x0000;
 	}
 	
@@ -110,7 +120,7 @@ void can_init( can_speed_typedef can_spd,  can_std_typedef can_typ )
 	CAN_FilterInit(&CAN_FilterInitStructure);
 
 	//Last Filter number ==> save all unknow ID to FIFO 1
-	CAN_FilterInitStructure.CAN_FilterNumber = 1;//测试直接设置为13 ？
+	CAN_FilterInitStructure.CAN_FilterNumber = 13;
 	CAN_FilterInitStructure.CAN_FilterMode = CAN_FilterMode_IdMask;
 	CAN_FilterInitStructure.CAN_FilterScale = CAN_FilterScale_32bit;
 	CAN_FilterInitStructure.CAN_FilterIdHigh = 0x0000;
@@ -118,124 +128,11 @@ void can_init( can_speed_typedef can_spd,  can_std_typedef can_typ )
 	CAN_FilterInitStructure.CAN_FilterMaskIdHigh = 0x0000;
 	CAN_FilterInitStructure.CAN_FilterMaskIdLow = 0x0000;
 	CAN_FilterInitStructure.CAN_FilterFIFOAssignment = 1;
-	CAN_FilterInitStructure.CAN_FilterActivation = ENABLE;
+	//CAN_FilterInitStructure.CAN_FilterActivation = ENABLE;
+	CAN_FilterInitStructure.CAN_FilterActivation = DISABLE;
 	CAN_FilterInit(&CAN_FilterInitStructure);
 
 	/* Transmit */
-	TxMessage.StdId = 0x7FE;//11bit 的仲裁域，即标识符，越低优先级越高, 0 to 0x7FF
-	TxMessage.ExtId = 0x19ABCDEF; //扩展帧
-	TxMessage.RTR = CAN_RTR_DATA;//远程发送请求位。如果这个帧是数据帧，则该位为0，
-								 //如果是远程帧，则为1。
-	TxMessage.IDE = CAN_ID_STD;  //0 表示这个标准帧；IDE=1 表示是扩展帧
-	//TxMessage.IDE = CAN_ID_EXT;  //0 表示这个标准帧；IDE=1 表示是扩展帧
-	
-	TxMessage.DLC = 1;//数据帧的字节数，0~8，数据域（Data Field）的长度
-
-	/* Enable Interrupt for receive FIFO 0 and FIFO overflow */
-	CAN_ITConfig(CAN1,CAN_IT_FMP0 | CAN_IT_FOV0, ENABLE);
-
-	/* Enable Interrupt for receive FIFO 1 and FIFO overflow */
-	CAN_ITConfig(CAN1,CAN_IT_FMP1 | CAN_IT_FOV1, ENABLE);	
-
-	//Enable CAN transceiver power
-	CAN_PM_ON;
-}
-
-void can_init1( )
-{
-	GPIO_InitTypeDef  GPIO_InitStructure;
-	CAN_InitTypeDef        CAN_InitStructure;
-	CAN_FilterInitTypeDef  CAN_FilterInitStructure;
-
-	// GPIO clock enable, had been init in uart1_init
-    //RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO | RCC_APB2Periph_GPIOA , ENABLE);
-	
-	/* Configure CAN pin: RX */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-	
-	/* Configure CAN pin: TX */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-	
-	/* CANx Periph clock enable */
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_CAN1, ENABLE);
-
-	/* CAN register init */
-	CAN_DeInit(CAN1);
-	CAN_StructInit(&CAN_InitStructure);
-
-	/* CAN cell init */
-	CAN_InitStructure.CAN_TTCM = DISABLE; //time triggered communication mode
-	//CAN_InitStructure.CAN_ABOM = DISABLE; //automatic bus-off management
-	CAN_InitStructure.CAN_ABOM = ENABLE; //EN:离线后，自动开启恢复过程
-	CAN_InitStructure.CAN_AWUM = DISABLE; //automatic wake-up mode
-	//CAN_InitStructure.CAN_NART = DISABLE; //no-automatic retransmission mode
-	CAN_InitStructure.CAN_NART = ENABLE; //EN:只发一次，不管结果；DIS: 自动重传，直到成功
-	//CAN_InitStructure.CAN_RFLM = DISABLE; //Receive FIFO Locked mode
-	CAN_InitStructure.CAN_RFLM = ENABLE; //EN: 溢出时丢弃新报文, DIS: 保留新报文
-	//CAN_InitStructure.CAN_TXFP = DISABLE; //transmit FIFO priority
-	CAN_InitStructure.CAN_TXFP = ENABLE; //EN: 优先级由发送顺序决定, DIS：优先级由报文ID决定
-	// CAN_Mode_Normal             ((uint8_t)0x00)  /*!< normal mode */
-	// CAN_Mode_LoopBack           ((uint8_t)0x01)  /*!< loopback mode */
-	// CAN_Mode_Silent             ((uint8_t)0x02)  /*!< silent mode */
-	// CAN_Mode_Silent_LoopBack    ((uint8_t)0x03)  /*!< loopback combined with silent mode */
-	//CAN_InitStructure.CAN_Mode = CAN_Mode_Silent_LoopBack; //CAN work mode
-	CAN_InitStructure.CAN_Mode = CAN_Mode_Normal; //CAN work mode
-
-	
-	//Baud = 24M(APB1) / (Prescaler) / 8(TqCount)	 								
-	CAN_InitStructure.CAN_SJW=CAN_SJW_2tq; //重同步单元
-	CAN_InitStructure.CAN_BS1=CAN_BS1_3tq;//采样点前
-	CAN_InitStructure.CAN_BS2=CAN_BS2_4tq;//采用点后
-	CAN_InitStructure.CAN_Prescaler = 12 ;//250K
-	//CAN_InitStructure.CAN_Prescaler = 6  ;//500K
-
-	CAN_Init(CAN1, &CAN_InitStructure);
-
-	/* CAN filter init */
-	//1, 共有14组过滤器(0~13),每组有2个32位寄存器：CAN_FxR0,CAN_FxR1
-	//2, CAN_FMR的FBMx位，设置过滤器工作在屏蔽模式(0)或列表模式(1)
-	//3, CAN_FilterFIFOAssignment 决定报文存去FIFO0 或 FIFO1
-	//4, 目前策略：已知的ID，通过屏蔽模式列出，保存到FIFO0
-	//   未知的ID，全部保存到 FIFO1，将来上传到服务器上供分析
-
-	//Filter number 0
-	CAN_FilterInitStructure.CAN_FilterNumber = 0;
-	
-	//CAN_FilterMode: CAN_FilterMode_IdMask or CAN_FilterMode_IdList
-	CAN_FilterInitStructure.CAN_FilterMode = CAN_FilterMode_IdMask;
-
-	//CAN_FilterScale: CAN_FilterScale_16bit or CAN_FilterScale_32bit
-	CAN_FilterInitStructure.CAN_FilterScale = CAN_FilterScale_32bit;
-
-	//0x0000 ~ 0xFFFF
-	CAN_FilterInitStructure.CAN_FilterIdHigh = (0x7FD)<<5;//11 bits,左对齐
-	CAN_FilterInitStructure.CAN_FilterIdLow = 0x0000;
-	CAN_FilterInitStructure.CAN_FilterMaskIdHigh = 0x7FF0;
-	CAN_FilterInitStructure.CAN_FilterMaskIdLow = 0x0000;
-
-	//FIFOAssignment: 0 or 1
-	CAN_FilterInitStructure.CAN_FilterFIFOAssignment = 0;
-	CAN_FilterInitStructure.CAN_FilterActivation = ENABLE;
-	CAN_FilterInit(&CAN_FilterInitStructure);
-
-	//Filter number 1
-	CAN_FilterInitStructure.CAN_FilterNumber = 1;
-	CAN_FilterInitStructure.CAN_FilterMode = CAN_FilterMode_IdMask;
-	CAN_FilterInitStructure.CAN_FilterScale = CAN_FilterScale_32bit;
-	CAN_FilterInitStructure.CAN_FilterIdHigh = 0x0000;
-	CAN_FilterInitStructure.CAN_FilterIdLow = 0x0000;
-	CAN_FilterInitStructure.CAN_FilterMaskIdHigh = 0x0000;
-	CAN_FilterInitStructure.CAN_FilterMaskIdLow = 0x0000;
-	CAN_FilterInitStructure.CAN_FilterFIFOAssignment = 1;
-	CAN_FilterInitStructure.CAN_FilterActivation = ENABLE;
-	CAN_FilterInit(&CAN_FilterInitStructure);
-
-	/* Transmit , will be remove */
 	TxMessage.StdId = 0x7FE;//11bit 的仲裁域，即标识符，越低优先级越高, 0 to 0x7FF
 	TxMessage.ExtId = 0x19ABCDEF; //扩展帧
 	TxMessage.RTR = CAN_RTR_DATA;//远程发送请求位。如果这个帧是数据帧，则该位为0，
@@ -297,10 +194,11 @@ bool can_send( can_std_typedef can_typ, frame_typedef frame_typ, u32 can_id, u8 
 		}
 		else {//end this loop
 			i = 0 ;
+			debug_obd("CAN send to %X OK\r\n", can_id);
 			return true;
 		}
 	}
-	
+	debug_obd("CAN send to %X ERR\r\n", can_id);
 	return false;
 }
 
@@ -351,7 +249,6 @@ void USB_LP_CAN1_RX0_IRQHandler(void) //for know CAN ID
 	}
 }
 
-
 /********************************************************************************
 * CAN1 receive FIFO 1 message pending Interrupt
 * params: void 
@@ -360,43 +257,31 @@ void USB_LP_CAN1_RX0_IRQHandler(void) //for know CAN ID
 void CAN1_RX1_IRQHandler(void) //for unknow CAN ID
 {	
 	unsigned char var_uchar;
-	CanRxMsg rx_msg;
 
-	CAN_Receive(CAN1,CAN_FIFO1, &rx_msg);
+	if( CAN_GetITStatus(CAN1, CAN_IT_FOV1 ) != RESET) { //FIFO0 overflow
+		//Release FIFO0, will lose data
+		CAN1->RF1R |= CAN_RF1R_RFOM1;
 
-	if (rx_msg.IDE == CAN_ID_STD) { 		
-		//unknow CAN STD ID, report to server
+		//report this error:  1,//find a empty record
 		for ( var_uchar = 0 ; var_uchar < MAX_WARN_MSG ; var_uchar++) {
 			if ( !my_icar.warn[var_uchar].msg ) { //empty msg	
 				//unsigned int msg;//file name(1 Byte), msg(1 Byte), line(2 B)
 				my_icar.warn[var_uchar].msg = (F_DRV_can) << 24 ;
-				my_icar.warn[var_uchar].msg |= UNK_CAN_STDID << 16 ;//unknow CAN STD ID
-				my_icar.warn[var_uchar].msg |= rx_msg.StdId ;
+				my_icar.warn[var_uchar].msg |= FIFO1_OF << 16 ;//FIFO1 overflow
+				my_icar.warn[var_uchar].msg |= __LINE__ ;
 				var_uchar = MAX_WARN_MSG ;//end the loop
 			}
-		}// 验证成功
+		}//8/20/2012 7:11:32 PM 验证成功
 	}
-	else {
-		//unknow CAN EXT ID, report to server
-		for ( var_uchar = 0 ; var_uchar < MAX_WARN_MSG ; var_uchar++) {
-			if ( !my_icar.warn[var_uchar].msg ) { //empty msg	
-				//unsigned int msg;//file name(1 Byte), msg(1 Byte), line(2 B)
-				my_icar.warn[var_uchar].msg = (F_DRV_can) << 24 ;
-				my_icar.warn[var_uchar].msg |= UNK_CAN_EXTID_H << 16 ;//Unknow ext-stand CAN ID high
-				my_icar.warn[var_uchar].msg |= ( rx_msg.ExtId >> 16) &0xFFFF ;
-				var_uchar = MAX_WARN_MSG ;//end the loop
-			}
-		}// 验证成功 2012/8/28 10:58:10
+	else { //receive a data from FIFO0
+		//不应在此次接收FIFO，因为有3个buffer可用，
+		//改在任务里接收，可充分利用buffer
+		//rx_msg_cnt0++;
+		CAN_ITConfig(CAN1,CAN_IT_FMP1, DISABLE);//disable FIFO1 FMP int
 
-		for ( var_uchar = 0 ; var_uchar < MAX_WARN_MSG ; var_uchar++) {
-			if ( !my_icar.warn[var_uchar].msg ) { //empty msg	
-				//unsigned int msg;//file name(1 Byte), msg(1 Byte), line(2 B)
-				my_icar.warn[var_uchar].msg = (F_DRV_can) << 24 ;
-				my_icar.warn[var_uchar].msg |= UNK_CAN_EXTID_L << 16 ;//Unknow ext-stand CAN ID low
-				my_icar.warn[var_uchar].msg |= ( rx_msg.ExtId ) &0xFFFF ;
-				var_uchar = MAX_WARN_MSG ;//end the loop
-			}
-		}// 验证成功 2012/8/28 10:58:08
+		//Send semaphore: 在任务中接收FIFO
+		OSIntEnter(); OSSemPost( sem_obd ); OSIntExit();
+		//CAN_Receive(CAN1,CAN_FIFO0, &RxMessage);
+		//CAN_ClearITPendingBit(CAN1, CAN_IT_FMP0);
 	}
 }
-
