ce7b993a8455f7dd62db89ffbc430555566027e0
cn0086.info <cn0086.info@gmail.com>
2012-12-24 07:58:15 +0000

    Add v01

---
+++ rokko/daemon/v01/log.c
@@ -0,0 +1,91 @@
+/**
+ *      log - Copyright (c) 2011-2012 cn0086 <cn0086.info@gmail.com>
+ *
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $URL: https://icar-firmware.googlecode.com/svn/rokko/daemon/v00/log.c $
+ *      $Rev: 315 $, $Date: 2012-12-17 19:39:33 +0800 (Mon, 17 Dec 2012) $
+ */
+
+#include "config.h"
+#include "log.h"
+
+extern int foreground, listen_port;
+
+char logdir[EMAIL+1], logname[EMAIL+1];
+
+int log_init( char* path )
+{
+	DIR *d; 
+
+	logdir[0] = logname[0] ='\0';
+
+	if ( strlen(path) > EMAIL - 30 ) { //too long path
+		printf("too long path: %s!!!\n",path);
+		return -1;
+	}
+	
+	strcat(logdir, path);
+	strcat(logdir, "log");
+	
+	if(!(d = opendir(logdir))){
+		//try to create dir
+		if ( mkdir(logdir, 0644) ) { //failure
+			printf("error opendir %s !!!\n",logdir);
+			return -1;
+		}
+		else { 
+			printf("Create log dir %s ok\n\n",logdir);
+		}
+	}
+
+	closedir(d);
+	return 0 ;
+}
+
+int log_save(FILE *log, char *msg)
+{
+	struct timeval tv;
+	char logtime[EMAIL+1] , newname[EMAIL+1];
+
+	gettimeofday(&tv,NULL);
+
+	strftime(logtime,EMAIL,"/%Y%m%d",(const void *)localtime(&tv.tv_sec));
+	
+	strcat(newname,logdir);strcat(newname,logtime);
+	snprintf(&newname[strlen(newname)],EMAIL,"_%d.txt",listen_port);
+
+	strftime(logtime,EMAIL,"%T ",(const void *)localtime(&tv.tv_sec));
+	
+	
+	if ( log ) { 
+		fprintf(log, "%s", logtime);
+		fprintf(log, "%s", msg);
+		fflush(log);
+	}		
+
+	if ( foreground ) printf("%s %s",logtime,msg);
+
+	return 0;	
+}
+
+int log_err( char *msg)
+{
+	struct timeval tv;
+	char logtime[EMAIL+1];
+	FILE *fp;
+	
+	gettimeofday(&tv,NULL);
+
+	strftime(logtime,EMAIL,"/%Y%m%d %T ",(const void *)localtime(&tv.tv_sec));
+	
+	fp = fopen("/tmp/log/log_err.txt", "a");
+	if ( fp ) { 
+		fprintf(fp, "%s", logtime);
+		fprintf(fp, "%s", msg);
+		fflush(fp);
+		fclose(fp) ;
+	}
+
+	return 0;	
+}
---
+++ rokko/daemon/v01/misc.h
@@ -0,0 +1,10 @@
+#ifndef _MISC_H
+#define _MISC_H
+
+/* misc.c */
+
+int	 a2port(const char *);
+void get_sysinfo( char *buf, int buf_len );
+
+#endif /* _MISC_H */
+
