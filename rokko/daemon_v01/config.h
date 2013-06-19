c7fea1035a77aae888edb6832a4ae8c572714222
cn0086.info <cn0086.info@gmail.com>
2013-06-19 09:13:49 +0000

    add db

--- rokko/daemon_v01/config.h
+++ rokko/daemon_v01/config.h
@@ -32,64 +32,64 @@
 
 #include <mysql.h>
 
-#define	MAXCLIENT 			5000
-#define PRODUCT_SN_LEN		8		//IMEI: 123456789012345 ==> 0x01 0x23 0x45 ... 0x45
+#define	MAXCLIENT 					5000
+#define PRODUCT_SN_LEN				8		//IMEI: 123456789012345 ==> 0x01 0x23 0x45 ... 0x45
 
-#define	EMAIL				128
-#define BUFSIZE 			2048	//2KB
+#define	EMAIL						128
+#define BUFSIZE 					2048	//2KB
 
-#define	LOG_DIR				"/root/"
+#define	LOG_DIR						"/root/"
 
-#define BACKLOG 5        //Maximum queue of pending connections
+#define BACKLOG 5        			//Maximum queue of pending connections
 
-#define PERIOD_CHECK_DB		3*60	//check database every 3*60 seconds
-#define PERIOD_SEND_MAIL	3*60*60	//send mail every 1*60*60 seconds
+#define PERIOD_CHECK_DB				3*60	//check database every 3*60 seconds
+#define PERIOD_SEND_MAIL			3*60*60	//send mail every 1*60*60 seconds
 
-//#define PERIOD_CHECK_DB		10		//check database every 3*60 seconds
-//#define PERIOD_SEND_MAIL	30*60	//send mail every 1*60*60 seconds
-#define PERIOD_SAVE_FILE	2*60	//Force save if timeout
-#define FORCE_SAVE_FILE		1		//Force save flag
+//#define PERIOD_CHECK_DB			10		//check database every 3*60 seconds
+//#define PERIOD_SEND_MAIL			30*60	//send mail every 1*60*60 seconds
+#define PERIOD_SAVE_FILE			2*60	//Force save if timeout
+#define FORCE_SAVE_FILE				1		//Force save flag
 
-#define NOTICER_ADDR		"cn0086.info@gmail.com"
-#define EMERGENCY_ADDR		"cn0086@139.com"
+#define NOTICER_ADDR				"cn0086.info@gmail.com"
+#define EMERGENCY_ADDR				"cn0086@139.com"
 
 //发送命令后，如果在timeout内无响应，重发
 //超过5*timeout后，清除此指令
 //对于 UPGRADE，则放宽处理
-#define CMD_TIMEOUT				5	//5秒
-#define MAX_CMD_QUEUE			10
+#define CMD_TIMEOUT					5	//5秒
+#define MAX_CMD_QUEUE				10
 
 //For GSM <==> Server protocol, need to same as STM32 firmware define
-#define GSM_HEAD				0xDE
-#define GSM_CMD_CONSOLE			0x43 //'C', Console command
-#define GSM_CMD_GPS				0x47 //'G', upload GPS information to server
-#define GSM_CMD_ERROR			0x45 //'E', Upload err log
-#define GSM_CMD_LOGIN			0x4C //'L', Login
-#define GSM_CMD_RECORD			0x52 //'R', record gsm/adc data
-#define GSM_CMD_UPGRADE			0x55 //'U', Upgrade firmware
-#define GSM_CMD_WARN			0x57 //'W', warn msg, report to server
+#define GSM_HEAD					0xDE
+#define GSM_CMD_CONSOLE				0x43 //'C', Console command
+#define GSM_CMD_GPS					0x47 //'G', upload GPS information to server
+#define GSM_CMD_ERROR				0x45 //'E', Upload err log
+#define GSM_CMD_LOGIN				0x4C //'L', Login
+#define GSM_CMD_RECORD				0x52 //'R', record gsm/adc data
+#define GSM_CMD_UPGRADE				0x55 //'U', Upgrade firmware
+#define GSM_CMD_WARN				0x57 //'W', warn msg, report to server
 
 //For Console command
-#define CONSOLE_CMD_NONE		0	 //no cmd
-#define CONSOLE_CMD_LIST_ALL	0x4C //'L', List all
-#define CONSOLE_CMD_LIST_SPE	0x6C //'l', List special
+#define CONSOLE_CMD_NONE			0	 //no cmd
+#define CONSOLE_CMD_LIST_ALL		0x4C //'L', List all
+#define CONSOLE_CMD_LIST_SPE		0x6C //'l', List special
 
 //For firmware upgrade
 //#define MAX_FW_SIZE				61450 //60*1024+10
-#define MAX_FW_SIZE				102400 //100*1024
-#define MIN_FW_SIZE				40960 //40*1024
+#define MAX_FW_SIZE					102400 //100*1024
+#define MIN_FW_SIZE					40960 //40*1024
 
 //For return error define, server ==> client
-#define ERR_RETURN_NONE			0	//00: No err
-#define ERR_RETURN_NO_LOGIN		1	//01: login failure
-#define ERR_RETURN_SRV_BUSY		2	//02: server busy
-#define ERR_RETURN_CRC_ERR		3	//03: CRC error
-#define ERR_RETURN_FW_LATEST	4	//04: latest firmware, no need upgrade
-#define ERR_RETURN_HW_ERR		5	//05: hardware no support
-#define ERR_RETURN_CONSOLE_CMD	6	//执行console命令时出错
-#define ERR_RETURN_LOGIN_CNT	7	//login count too many
-#define ERR_RETURN_NO_SN		8	//No serial number
-#define ERR_UNKNOW				0xFF//未知错误
+#define ERR_RETURN_NONE				0	//00: No err
+#define ERR_RETURN_NO_LOGIN			1	//01: login failure
+#define ERR_RETURN_SRV_BUSY			2	//02: server busy
+#define ERR_RETURN_CRC_ERR			3	//03: CRC error
+#define ERR_RETURN_FW_LATEST		4	//04: latest firmware, no need upgrade
+#define ERR_RETURN_HW_ERR			5	//05: hardware no support
+#define ERR_RETURN_CONSOLE_CMD		6	//执行console命令时出错
+#define ERR_RETURN_LOGIN_CNT		7	//login count too many
+#define ERR_RETURN_NO_SN			8	//No serial number
+#define ERR_UNKNOW					0xFF//未知错误
 
 //record index define, need same as client main.h define
 #define REC_IDX_ADC1				10 //for ADC1
@@ -117,12 +117,12 @@
 //'43' ==> Server
 
 //#define	CLOUD_HOST				"cn0086.info"
-#define	CLOUD_HOST				"yun.test.33xuexi.com"
+#define	CLOUD_HOST					"yun.test.33xuexi.com"
 
 //For database
-#define	DB_HOST					"localhost"
-#define	DB_NAME					"rokko_v00"
-#define	DB_USER					"root"
-#define	DB_PWD					"cn0086"
+#define	DB_HOST						"localhost"
+#define	DB_USER						"root"
+#define	DB_PWD						"cn0086"
+#define	DB_NAME						"rokko_v00"
 
 #endif /* _CONFIG_H */
---
+++ rokko/daemon_v01/database.c
@@ -0,0 +1,68 @@
+//$URL: https://icar-firmware.googlecode.com/svn/rokko/daemon_v01/console.c $ 
+//$Rev: 364 $, $Date: 2013-05-09 19:20:16 +0800 (Thu, 09 May 2013) $
+
+#include "config.h"
+#include "rokkod.h"
+
+extern unsigned int foreground;
+
+//Connect to MYSQ
+//return: 0 success
+//        1 failure
+int db_connect(struct db_struct *rokko_db)
+{
+	int err;
+	char create_db[EMAIL];
+
+	if ( foreground ) {
+		fprintf(stderr, "Initializing mysql ... \n");
+	}
+
+	if ( !mysql_init(&(rokko_db->mysql)) )
+	{
+		if ( foreground ) {
+			fprintf(stderr, "Initializing mysql failure, return\n");
+		}
+		return 1;
+	}
+
+	//fprintf(stderr, "DB connect %s:%d\n",__FILE__,__LINE__);
+	
+	if (!mysql_real_connect(&(rokko_db->mysql),\
+							rokko_db->db_host,\
+							rokko_db->db_user,\
+							rokko_db->db_pwd,\
+							rokko_db->db_name,\
+							0,NULL,0))	{
+		err = mysql_errno(&(rokko_db->mysql)) ;
+		if ( err == 1049 ) { //Unknown database, create it
+			if (!mysql_real_connect(&(rokko_db->mysql),
+							rokko_db->db_host,\
+							rokko_db->db_user,\
+							rokko_db->db_pwd,\
+							"mysql",0,NULL,0)) {
+			//connect with default database:mysql still failure
+				return 1 ;
+			}
+
+			if ( foreground ) {
+				fprintf(stderr, "Create database icar_v03 ...\n");
+			}
+			mysql_query(&(rokko_db->mysql),"create database icar_v03;");
+			mysql_query(&(rokko_db->mysql),"use icar_v03;");
+		}
+		else { //others error
+			fprintf(stderr, "Error: %d, meaning:%s\r\n", err,mysql_error(&(rokko_db->mysql)));
+			return 1;
+		}
+	}
+
+	//本次连接使用的默认字符集为utf8
+	mysql_query( &(rokko_db->mysql) , "SET NAMES 'utf8'");
+
+	if ( foreground ) {
+		;//fprintf(stderr, "Connectiong to mysql ok.\n");
+	}
+
+	return 0 ;
+}
