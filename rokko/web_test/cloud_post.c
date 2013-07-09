830328256fe2b6cb644885b90bc530d627b55ede
cn0086.info <cn0086.info@gmail.com>
2013-07-09 02:59:57 +0000

    add web test

--- rokko/web_test/cloud_post.c
+++ rokko/web_test/cloud_post.c
@@ -206,11 +206,18 @@ int cloud_post( char *remote_host, char *request, int port )
 
 	cloud_tcpclient_create(&client,remote_host,port);
 	
-	if(http_post(&client,remote_host,"/service/equipment/?",request,&response)){
+	//http://yun.test.33xuexi.com/service/state/?
+	if(http_post(&client,remote_host,"/service/state/?",request,&response)){
 		fprintf(stderr,"Error! check %s:%d\n",__FILE__, __LINE__);
 		return 1;
 	}
 
+/*
+	if(http_post(&client,remote_host,"/service/equipment/?",request,&response)){
+		fprintf(stderr,"Error! check %s:%d\n",__FILE__, __LINE__);
+		return 1;
+	}
+*/
 	ret = strncmp(response,"1000",4);
 	if ( foreground ) {
 		fprintf(stderr,"\nReturn: %s\t%d\n",response,ret);
@@ -225,7 +232,11 @@ int cloud_post( char *remote_host, char *request, int port )
 int main(int argc, char *argv[]) {
 	
 	unsigned char post_buf[BUFSIZE], host_info[BUFSIZE], logtime[EMAIL];
-	unsigned int i;
+	unsigned int i, lat, lon;
+	unsigned int lat_ori=0, lon_ori=0, lat_offset=0, lon_offset=0 ;
+	unsigned int lat_degree, lat_minute, lat_second;
+	unsigned int lon_degree, lon_minute, lon_second;
+
 	struct timeval log_tv;
 	unsigned int usecs=50000;
 
@@ -235,6 +246,35 @@ int main(int argc, char *argv[]) {
 	gettimeofday(&log_tv,NULL);
 	strftime(logtime,EMAIL,"%Y-%m-%d %T",(const void *)localtime(&log_tv.tv_sec));
 
+	lat_ori = (24*60+((getpid())&0x3F))*30000;
+	lon_ori = (114*60+((getpid())&0x3F))*30000;
+
+	while ( 1 ) {//for test upload data
+		lat = lat_ori + lat_offset ;	lat_offset = lat_offset + 33*((getpid())&0x3F) ;
+		lon = lon_ori + lon_offset ;	lon_offset = lon_offset + 47*((getpid())&0x7F) ;
+
+		lat_degree = lat/(60*30000);
+		lat_minute = (lat - lat_degree*60*30000)/30000;
+		lat_second = (lat - lat_degree*60*30000 - lat_minute*30000)/3;
+
+		lon_degree = lon/(60*30000);
+		lon_minute = (lon - lon_degree*60*30000)/30000;
+		lon_second = (lon - lon_degree*60*30000 - lon_minute*30000)/3;
+
+		snprintf(post_buf,BUFSIZE,"sn=997755331160687&longitude=%03d%02d.%04d&latitude=%02d%02d.%04d&voltage=10.8",\
+									lon_degree,lon_minute,lon_second,\
+									lat_degree,lat_minute,lat_second);
+
+		//post to cloud
+		if ( cloud_post( CLOUD_HOST, post_buf, 80 ) != 0 ) {//error
+			fprintf(stderr,"%s\nErr, exit.\n",post_buf);
+			//exit( 0 );
+			usleep(usecs*60);
+		}
+		usleep(usecs);	
+	}
+	
+	//For test initial SN
 	//模拟终端序列号：9977553311 (10位固定前续) + xxxxx (5位随机数)
 	//470 ==> 10726
 	//10726 ==> 15722
